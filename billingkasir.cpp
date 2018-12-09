dbe9c761dccf0ee394918e94f29526d87b4eb2b5
andhika bagus setiawan <45529925+andhikabagussetiawan@users.noreply.github.com>
2018-12-09 15:29:42 +0700

    Create billingkasir.cpp

---
+++ billingkasir.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int pilihan;
+    double p,np,Tot,etj,aj;
+    cout<<"SELAMAT DATANG DIRUMAH MAKAN SELERA RAKYAT"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"daftar menu makanan"<<endl;
+    cout<<"___________________"<<endl;
+    cout<<"1.paket hemat\n";
+    cout<<"2.paket mantap\n";
+    cout<<"3.paket jumbo\n";
+    cout<<"4.paket double jumbo\n";
+    cout<<"5.paket family\n";
+    cout<<endl;
+    cout<<"masukan menu makanan :";
+    cin>>pilihan;
+    cout<<endl;
+
+    switch(pilihan)
+    {
+
+        case
+    }
+}
